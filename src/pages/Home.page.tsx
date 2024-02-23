c2a40761d08a29aab5053150708b729074a5b76d
UsmanS2 <twixmixer42@gmail.com>
2024-02-23 15:25:00 -0600

    working on header

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,26 @@
+import { AppShell } from '@mantine/core';
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { HeaderMenu } from '@/components/Header/HeaderMenu';
 
 export function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+    <AppShell
+    header={{height: 56}}>
+      <AppShell.Header>
+        <HeaderMenu />
+      </AppShell.Header>
+      <AppShell.Main>
+        <div id = "Welcome">
+        <Welcome />
+        <ColorSchemeToggle />
+        </div>
+        <Welcome />
+        <Welcome />
+        <Welcome />
+      </AppShell.Main>
+      </AppShell>
     </>
   );
 }
