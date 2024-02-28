c53aeb6c303f2416b5fe46f4e59c5be817e4b571
UsmanS2 <twixmixer42@gmail.com>
2024-02-27 18:26:25 -0600

    fixed spacing on welcome

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -4,13 +4,13 @@ import classes from './Welcome.module.css';
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="left" mt={100}>
+      <Title className={classes.title} ta="left" mt={100} ml={150}>
         Hi, I'm {' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
           Usman Shafi
         </Text>
       </Title>
-      <Text c="dimmed" ta="left" size="lg" maw={580} mx="md" mt="xl" style={{ paddingLeft: '200px' }}>
+      <Text c="dimmed" ta="left" size="lg" maw={580} mx="md" mt="xl" ml={250}>
         Business Computer Information Systems Student focused on learning, growing, and thriving{' '}
         
       </Text>
