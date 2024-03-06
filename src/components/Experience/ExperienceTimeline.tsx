63e46c8664d14f5298b6e16aa5404d990639ff6d
UsmanS2 <twixmixer42@gmail.com>
2024-03-06 00:42:46 -0600

    working more on the timeline

--- src/components/Experience/ExperienceTimeline.tsx
+++ src/components/Experience/ExperienceTimeline.tsx
@@ -9,25 +9,25 @@ function ExperienceTimeline() {
   const hoverRef4 = useHover();
   
   return (
-    <Timeline active={4} bulletSize={24} lineWidth={2} ml={rem(150)}>
+    <Timeline active={4} bulletSize={24} lineWidth={2} mx={rem(350)}>
       <Timeline.Item bullet={<IconGitBranch size={12} />} title="How it started" ref={ref}>
         <Text c="dimmed" size="sm">Took my first programming class </Text>{/*<Text variant="link" component="span" inherit>fix-notifications</Text> from master</Text>*/}
         {hovered ? <><Text>yes</Text></> : <><Text></Text></>}
         <Text size="xs" mt={4}>2017-2018</Text>
       </Timeline.Item>
 
-      <Timeline.Item bullet={<IconGitCommit size={12} />} title="My First Job" ref={hoverRef1.ref}>
+      <Timeline.Item bullet={<IconGitCommit size={12} />} title="My First Job"  ref={hoverRef1.ref} >
         <Text c="dimmed" size="sm">Started working at Code Ninjas <Text variant="link" component="span" inherit></Text></Text>
         {hoverRef1.hovered ? <><Text>yes</Text></> : <><Text>no</Text></>}
-        <Text size="xs" mt={4}>June 2018</Text>
+        <Text size="xs" mt={4}>June 2018 - October 2019</Text>
       </Timeline.Item>
 
-      <Timeline.Item title="My Second Job" bullet={<IconGitPullRequest size={12} />} lineVariant="dashed">
+      <Timeline.Item title="My Second Job" bullet={<IconGitPullRequest size={12} />} >
         <Text c="dimmed" size="sm">Started working at theCoderSchool<Text variant="link" component="span" inherit></Text></Text>
-        <Text size="xs" mt={4}>October 2020</Text>
+        <Text size="xs" mt={4}>October 2020 - Present</Text>
       </Timeline.Item>
 
-      <Timeline.Item title="Code review" bullet={<IconMessageDots size={12} />}>
+      <Timeline.Item title="Management Experience" bullet={<IconMessageDots size={12} />}>
         <Text c="dimmed" size="sm">Spent a summer as the Interim General Manager<Text variant="link" component="span" inherit></Text></Text>
         <Text size="xs" mt={4}>May 2023 - August 2023</Text>
       </Timeline.Item>
